e8a40fdc21697332ea012f5bcab4312459f9ff89
ptakchun <ptakchun@connect.hku.hk>
2020-10-09 19:14:27 +0800

    finish added login page, not yet tested with backend API, using dummy login logic

--- frontend/reactapp/src/App.js
+++ frontend/reactapp/src/App.js
@@ -2,29 +2,34 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from './Login';
+import $ from 'jquery'; 
+
+const APIURL = 'http://localhost:3002/cfg-apac-team29/us-central1/api/';
 
 class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
         isLoggedIn: false,
-        
+        role: '',
     };
     this.handleLoginSubmit = this.handleLoginSubmit.bind(this);
   } 
+  
 
-  handleLoginSubmit(event) { 
+  handleLoginSubmit(event,role) { 
     event.preventDefault();
     this.setState({
-        isLoggedIn: true,
+      isLoggedIn:true,
+      role: role,
     });
-    console.log(event.target.value);
+    
   }
 
   render() {
     let userInterface = null;
     if(this.state.isLoggedIn){
-      userInterface = <h1>logged In</h1>;
+    userInterface = <h1>logged In As {this.state.role}</h1>;
     }else{
       userInterface = <Login handleLoginSubmit={this.handleLoginSubmit} />;
     }
--- frontend/reactapp/src/Login.js
+++ frontend/reactapp/src/Login.js
@@ -3,13 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 import { useMediaQuery } from 'react-responsive';
 import $ from 'jquery'; 
+import { Form, Input, Button, Checkbox } from 'antd';
+import 'antd/dist/antd.css';
+import './Login.css';
 
+// const isDesktopOrLaptop = useMediaQuery({query: '(min-device-width: 1224px)'});
+// const isBigScreen = useMediaQuery({ query: '(min-device-width: 1824px)' });
+// const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' });
+// const isTabletOrMobileDevice = useMediaQuery({query: '(max-device-width: 1224px)'});
+// const isPortrait = useMediaQuery({ query: '(orientation: portrait)' });
+// const isRetina = useMediaQuery({ query: '(min-resolution: 2dppx)' });
+
+const APIURL = 'http://localhost:3002/cfg-apac-team29/us-central1/api/';
 
 class Login extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            isLoggedIn: this.props.isLoggedIn,
             isLoginFailed: false,
             username: '',
             password: '',
@@ -23,11 +33,38 @@ class Login extends React.Component {
 
     handleLoginSubmit(event){
         event.preventDefault();
-        let isVerified = this.state.username == '123';
         //ADD CODE: ajax API here to check if the user exist
-
+        // $.ajax({
+        //     type:'POST',
+        //     url: APIURL+'login',
+        //     dataType: 'json',
+        //     data: {
+        //         username: this.state.username,
+        //         password: this.state.password
+        //       },
+        //     xhrFields: {withCredentials: true},
+        //     success: function(data) {
+        //         console.log('print data:',data);                
+        //         if(data.status == 'success'){
+        //             this.props.handleLoginSubmit(event, data.role);
+        //             this.setState({
+        //                 isLoginFailed:false,
+        //             });
+        //         }else{
+        //             this.setState({
+        //                 isLoginFailed:true
+        //             });
+        //         }
+        //         this.refresh();
+              
+        //     }.bind(this),
+        //     error: function(xhr, ajaxOptions, thrownError) {
+        //         alert(xhr.status, thrownError);
+        //     }.bind(this)
+        // });
+        let isVerified = this.state.username == '123';
         if(isVerified){
-            this.props.handleLoginSubmit(event);
+            this.props.handleLoginSubmit(event, 'Case Manager');
         }else{
             this.setState({
                 isLoginFailed:true
@@ -52,22 +89,29 @@ class Login extends React.Component {
             );
         }
     }
-    
+
+    showLoginPageTitle(){
+        return(
+            <div className='loginPageTitle'>
+            <h1>New Hope Mobile Matching System</h1>
+            </div>
+        );
+    }
     
 
     render(){
         return(
             <div className='Login'>
-                <form className="form_login" onSubmit={this.handleLoginSubmit}>
+                {this.showLoginPageTitle()}
+                <br/>
                     <label for="input_username">Username:</label>
                     <input id="input_username" type="text" value={this.username} onChange={this.onUsernameChange} />
                     <br/>
                     <label for="input_password">Password:</label>
                     <input id="input_password" type="password" value={this.password} onChange={this.onPasswordChange} />
                     <br/>
-                    <input id='login_sumbit' type="submit" value="Submit" />
-                </form>
-                {this.handleLoginFailed()}
+                    <Button type='primary' onClick={this.handleLoginSubmit}>Submit</Button>
+                {this.handleLoginFailed()}       
             </div>
         );
     }
