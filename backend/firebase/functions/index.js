5fc6051873f90ac535005264349e90454ff2f857
thermitex <thermitex@outlook.com>
2020-10-09 13:46:57 +0800

    Configure firebase

--- backend/firebase/functions/index.js
+++ backend/firebase/functions/index.js
@@ -1,9 +1,47 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const express = require("express");
+const cors = require("cors");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+const db = admin.firestore();
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get("/", (req, res) => {
+  return res.status(200).send("Firebase is working.");
+});
+
+app.post("/write-doc", async (req, res) => {
+  const { collection, documentId, documentValue } = req.body;
+  const docRef = db.collection(collection).doc(documentId);
+  try {
+    const r = await docRef.set(documentValue);
+    console.log("Write Success", r);
+    return res.status(200).end();
+  } catch (e) {
+    console.error("Write Failure", r);
+    return res.status(500).end();
+  }
+});
+
+app.get("/read-doc", async (req, res) => {
+  const { collection, documentId } = req.query;
+  const docRef = db.collection(collection).doc(documentId);
+  try {
+    const r = await docRef.get();
+    console.log("Read Success", r);
+    return res.status(200).send({
+      name: r.id,
+      value: r.data(),
+    });
+  } catch (e) {
+    console.error("Read Failure", e);
+    return res.status(500).end();
+  }
+});
+
+exports.api = functions.https.onRequest(app);
